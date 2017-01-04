7e36c20feab181f145cc826826f290177abd5834
Daniel Lara <fradalape@hotmail.com>
2017-01-04 15:08:52 -0600

    add facades storage

--- src/Controllers/Admin/SlideController.php
+++ src/Controllers/Admin/SlideController.php
@@ -8,24 +8,17 @@ class SlideController extends BaseController
 
 	public function index()
 	{
-		return view('blogify::admin.slide.new')
+		return view('blogify::admin.slide.new');
 	}
 
 	public function save(Request $request)
 	{
+	   $file = $request->file('file');
+       $name = $file->getClientOriginalName();
+	   $url =  \Storage::url($name);
+       \Storage::disk('local')->put($name,  \File::get($file));
 
-       //obtenemos el campo file definido en el formulario
-       $file = $request->file('file');
-
-       //obtenemos el nombre del archivo
-       $nombre = $file->getClientOriginalName();
-
-	   $url = Storage::url($nombre);
-
-       //indicamos que queremos guardar un nuevo archivo en el disco local
-       \Storage::disk('local')->put($nombre,  \File::get($file));
-
-       return "archivo guardado";
+       return ("archivo guardado");
 
 	}
    
