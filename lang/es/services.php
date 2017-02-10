d718e11b297536ac4649ce108c850ffea2d615d7
Daniel Lara <fradalape@hotmail.com>
2017-02-10 11:46:02 -0600

    translate services

---
+++ lang/es/services.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'services' => [
+        'title' => 'Agregar Servicio',
+    ],
+
+    'form' => [
+        'lbl' => [
+            'value1' => ' Nombre:',
+            'value2' => ' Descripción:',
+            'value3' => ' Seleccionar Icono:',
+            ],
+        'button' => ' Agregar'
+    ],
+];
